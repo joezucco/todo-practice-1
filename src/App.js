14d8fe8e077de200c59399377ca78d39571ae2af
Joe Zucco <josephzucco@gmail.com>
2021-10-04 18:20:14 -0400

    set state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import React, { useState } from "react";
 import "./App.css";
 //Importing Components
 import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className='App'>
       <header>
